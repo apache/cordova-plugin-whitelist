bd4b74a09ebf35c605a694128c2ef75d54cd2767
cleitontavares <cleitontavares@live.com>
2015-09-05 00:59:56 -0300

    remove alert msg in console for develop app

--- whitelist.js
+++ whitelist.js
@@ -19,9 +19,5 @@
 */
 
 if (!document.querySelector('meta[http-equiv=Content-Security-Policy]')) {
-    var msg = 'No Content-Security-Policy meta tag found. Please add one when using the cordova-plugin-whitelist plugin.';
-    console.error(msg);
-    setInterval(function() {
-        console.warn(msg);
-    }, 10000);
+
 }
