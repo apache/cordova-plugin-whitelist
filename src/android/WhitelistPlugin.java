ab36e6d19b3e1d6b6e24562cdb43980ef9d02df6
Rob Close <rob.close@sap.com>
2016-06-08 14:58:27 -0400

    CB-11391 Revert c2c0072 android: Does not pass sonarqube scan
    Originaly was trying to correct this sonarqube complaint:
    "Correctness - Suspicious reference comparison of Boolean values"
    
    I failed to notice that the condition can be one of three states:
    true, false, or null.
    
    The original code was valid. Undoing my change.
    
    This closes #21

--- src/android/WhitelistPlugin.java
+++ src/android/WhitelistPlugin.java
@@ -118,7 +118,7 @@ public class WhitelistPlugin extends CordovaPlugin {
 
     @Override
     public Boolean shouldAllowRequest(String url) {
-        if (shouldAllowNavigation(url)) {
+        if (Boolean.TRUE == shouldAllowNavigation(url)) {
             return true;
         }
         if (allowedRequests.isUrlWhiteListed(url)) {
